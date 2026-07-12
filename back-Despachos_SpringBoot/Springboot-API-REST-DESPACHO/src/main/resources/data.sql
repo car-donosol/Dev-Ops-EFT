692abff076b4c1b1ded41600dcd9a01490978559
car-donosol <car.donosol@duocuc.cl>
2026-07-11 20:18:48 -0400

    feat: H2 default, MySQL profile, data.sql seed, .gitattributes

---
+++ back-Despachos_SpringBoot/Springboot-API-REST-DESPACHO/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO despacho (fecha_despacho, patente_camion, intento, id_compra, direccion_compra, valor_compra, despachado) VALUES
+('2026-07-03', 'ABCD-12', 1, 1, 'Av. Libertador Bernardo O''Higgins 1234, Santiago', 250000, TRUE),
+('2026-07-04', 'EFGH-34', 1, 2, 'Calle Pedro Montt 567, Valparaíso', 180000, TRUE),
+('2026-07-08', 'IJKL-56', 1, 3, 'Av. Colón 890, Concepción', 320000, FALSE);
---
+++ back-Ventas_SpringBoot/Springboot-API-REST/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO venta (direccion_compra, valor_compra, fecha_compra, despacho_generado) VALUES
+('Av. Libertador Bernardo O''Higgins 1234, Santiago', 250000, '2026-07-01', TRUE),
+('Calle Pedro Montt 567, Valparaíso', 180000, '2026-07-02', TRUE),
+('Av. Colón 890, Concepción', 320000, '2026-07-03', FALSE),
+('Av. Valparaíso 1456, Viña del Mar', 95000, '2026-07-05', FALSE),
+('Calle Caupolicán 789, Temuco', 210000, '2026-07-07', FALSE);
